068e7e5bcdb2d5686ca2f7a7f5ba3f7376d5dcd7
yafik90 <82032788+yafik90@users.noreply.github.com>
2021-04-06 18:28:51 +0700

    Add files via upload

---
+++ rpgV3-master/src/Pintu.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+//belum menggunakan inheritance, idealnya turunan dari Item
+
+public class Pintu extends Item {
+
+    private ArrayList<String> arrAksi = new ArrayList<>();
+    private GameInfo objGameInfo;
+
+    //constrcutor
+    public Pintu(String nama) {
+        super(nama);
+        //init pilihan
+        arrAksi.add("Deskripsikan pintu");
+        arrAksi.add("Coba buka pintu");
+    }
+
+    public void prosesAksi(int subPil) {
+        //1: deskripsikan
+        //2: buka pintu
+        if (subPil==1) {
+            System.out.println("Pintu berwarna merah dengan tulisan 'EXIT' di atas ");
+        } else if (subPil==2) {
+            //cek apakah mempunyai kunci
+            if (objGameInfo.getObjPlayer().cariItem("Kunci")) {
+                //kunci ada, pintu terbuka
+                System.out.println("Player menggunakan kunci untuk membuka pintu dan pintu terbuka!");
+                objGameInfo.setGameOver(true); //game over
+            } else {
+                //kunci tidak ada
+                System.out.println("Player mencboa membuka pintu. TERKUNCI!");
+            }
+        }
+    }
+
+    @Override
+    public void setObjGameInfo(GameInfo objGameInfo) {
+        super.setObjGameInfo(objGameInfo);
+    }
+
+    public ArrayList<String> getAksi() {
+        return arrAksi;
+    }
+
+}
